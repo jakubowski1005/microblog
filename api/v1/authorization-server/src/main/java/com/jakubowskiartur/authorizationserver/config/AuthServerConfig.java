0ff6db8f758b0b2bb5fcd2f6a99ddb6ed76ff0d4
jakbuowski1005 <jakubowski1005@gmail.com>
2020-04-07 16:44:17 +0200

    Implement test auth server

---
+++ api/v1/authorization-server/src/main/java/com/jakubowskiartur/authorizationserver/config/AuthServerConfig.java
@@ -0,0 +1,52 @@
+package com.jakubowskiartur.authorizationserver.config;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+public class AuthServerConfig extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer {
+
+    @Bean
+    @SneakyThrows
+    public AuthenticationManager authenticationManager() {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+
+        security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    @SneakyThrows
+    public void configure(ClientDetailsServiceConfigurer clients) {
+
+        clients
+                .inMemory()
+                .withClient("web")
+                .secret(passwordEncoder().encode("webpass"))
+                .scopes("READ", "WRITE")
+                .authorizedGrantTypes("password", "authorization_code");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+
+        endpoints.authenticationManager(authenticationManager());
+    }
+}
--- api/v1/user-service/src/main/java/com/jakubowskiartur/userservice/UserServiceApplication.java
+++ api/v1/user-service/src/main/java/com/jakubowskiartur/userservice/UserServiceApplication.java
@@ -2,12 +2,21 @@ package com.jakubowskiartur.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String greeting() {
+		return "Hello from user service";
+	}
 }
