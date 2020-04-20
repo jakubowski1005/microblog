4d56eac34d365079fa0456304745adc638385edd
jakbuowski1005 <jakubowski1005@gmail.com>
2020-04-20 22:39:31 +0200

    Add daos

--- api/v1/bloget-v1/auth-service/src/main/java/com/jakubowskiartur/authservice/AuthServiceApplication.java
+++ api/v1/bloget-v1/auth-service/src/main/java/com/jakubowskiartur/authservice/AuthServiceApplication.java
@@ -3,11 +3,15 @@ package com.jakubowskiartur.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAuthorizationServer
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
