a674a4028e59eca35efb5533176c1397db5019df
jakbuowski1005 <jakubowski1005@gmail.com>
2020-04-26 03:48:48 +0200

    Add post repository

---
+++ api/v1/post-service/src/main/java/com/jakubowskiartur/postservice/util/StringSetConverter.java
@@ -0,0 +1,21 @@
+package com.jakubowskiartur.postservice.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Set;
+
+@Converter
+public class StringSetConverter implements AttributeConverter<Set<String>, String> {
+
+    private static final String SEPARATOR = ";";
+
+    @Override
+    public String convertToDatabaseColumn(Set<String> attribute) {
+        return String.join(SEPARATOR, attribute);
+    }
+
+    @Override
+    public Set<String> convertToEntityAttribute(String dbData) {
+        return Set.of(dbData.split(SEPARATOR));
+    }
+}
