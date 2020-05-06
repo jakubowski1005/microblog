2bcf4fdb046abb7d9453be819a4c471f273e9a6c
jakbuowski1005 <jakubowski1005@gmail.com>
2020-05-06 10:13:08 +0200

    Fix docker compose

--- trash/resource-server/src/main/java/com/jakubowskiartur/resourceserver/repository/ResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakubowskiartur.resourceserver.repository;
-
-import com.jakubowskiartur.resourceserver.model.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResourceRepository extends JpaRepository<Resource, Integer> {
-}
