25e3f67e08f7917e376293788e3e24e62a06fc34
jakbuowski1005 <jakubowski1005@gmail.com>
2020-05-19 15:57:07 +0200

    Change dir tree

--- api/v1/auth-service/src/main/java/com/jakubowskiartur/authservice/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.jakubowskiartur.authservice.service;
-
-import org.springframework.http.ResponseEntity;
-
-public interface AuthService {
-
-    ResponseEntity<?> register(SignUpRequest request);
-}
