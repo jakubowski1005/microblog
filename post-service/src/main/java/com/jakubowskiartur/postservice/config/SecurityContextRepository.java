f38382016de754b027d333f22a4530527e71c78b
Artur Jakubowski <jakubowski1005@gmail.com>
2021-01-06 20:34:59 +0100

    Add jwt security to post service

---
+++ post-service/src/main/java/com/jakubowskiartur/postservice/config/SecurityContextRepository.java
@@ -0,0 +1,42 @@
+package com.jakubowskiartur.postservice.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import static java.util.Objects.nonNull;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@AllArgsConstructor
+public class SecurityContextRepository implements ServerSecurityContextRepository {
+
+    private ReactiveAuthenticationManager authenticationManager;
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        String header = request.getHeaders().getFirst(AUTHORIZATION);
+
+        if (nonNull(header) && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(token, token);
+
+            return this.authenticationManager.authenticate(authentication).map(SecurityContextImpl::new);
+        }
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ post-service/src/main/java/com/jakubowskiartur/postservice/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.jakubowskiartur.postservice.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+
+    private ReactiveAuthenticationManager manager;
+    private ServerSecurityContextRepository contextRepository;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.exceptionHandling()
+                .authenticationEntryPoint((swe, e) ->
+                        Mono.fromRunnable(() -> swe.getResponse().setStatusCode(UNAUTHORIZED)))
+                .accessDeniedHandler((swe, e) ->
+                    Mono.fromRunnable(() -> swe.getResponse().setStatusCode(FORBIDDEN)))
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authenticationManager(manager)
+                .securityContextRepository(contextRepository)
+                .authorizeExchange()
+                .pathMatchers(OPTIONS).permitAll()
+                .pathMatchers("/posts", "/tags").permitAll()
+                .anyExchange().authenticated()
+                .and().build();
+    }
+}
