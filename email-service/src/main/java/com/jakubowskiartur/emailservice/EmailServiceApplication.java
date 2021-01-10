a867db334d1d39978c8eb4791f99297ad937f8a1
Artur Jakubowski <jakubowski1005@gmail.com>
2021-01-10 22:39:13 +0100

    Fix email service security

--- email-service/src/main/java/com/jakubowskiartur/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/com/jakubowskiartur/emailservice/EmailServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
 @RestController
 public class EmailServiceApplication {
