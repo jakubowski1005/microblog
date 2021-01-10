96d3ae1fecffdaf53779647bb155d26a085703cd
Artur Jakubowski <jakubowski1005@gmail.com>
2021-01-10 16:17:48 +0100

    Fix mongo

--- auth-service/src/main/java/com/jakubowskiartur/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/jakubowskiartur/authservice/AuthServiceApplication.java
@@ -2,9 +2,11 @@ package com.jakubowskiartur.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
